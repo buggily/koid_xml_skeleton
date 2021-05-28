4e22bd06a458c3a69b219fbabc329e2d183c6fc5
Lance Holsten <toholsten@gmail.com>
2021-05-28 01:03:21 -0500

    remove deprecated testing files

--- app/src/test/java/com/buggily/skeleton/InstantTaskExecutor.kt
+++
@@ -1,9 +0,0 @@
-package com.buggily.skeleton
-
-import androidx.arch.core.executor.TaskExecutor
-
-class InstantTaskExecutor : TaskExecutor() {
-    override fun isMainThread(): Boolean = true
-    override fun postToMainThread(runnable: Runnable) = runnable.run()
-    override fun executeOnDiskIO(runnable: Runnable) = runnable.run()
-}
